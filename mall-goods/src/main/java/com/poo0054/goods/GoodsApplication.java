1205e8b389ae71c8bedb5435ebd6737e457e93b3
poo0054 <poo0054@outlook.com>
2023-03-14 18:30:13 +0800

    add goods model

---
+++ mall-goods/src/main/java/com/poo0054/goods/GoodsApplication.java
@@ -0,0 +1,15 @@
+package com.poo0054.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangzhi
+ * @date 2023/3/14
+ */
+@SpringBootApplication
+public class GoodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class, args);
+    }
+}
